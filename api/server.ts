698f5adc887f33ce01a2f1300a146d676853d633
dontry <mccoy018@gmail.com>
2018-11-09 22:54:25 +1100

    helloworld test

--- api/server.ts
+++ api/server.ts
@@ -1,31 +1,39 @@
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import SwaggerExpress from "swagger-express-mw";
+import { getHello } from "./controllers/hello_world";
 import logger from "./utils/logger";
 
 dotenv.config({ path: ".env" });
+const port = process.env.PORT || 10010;
 
 const app = express();
-const config = {
-  appRoot: __dirname
-};
-
-SwaggerExpress.create(config, (err, swaggerExpress) => {
-  if (err) {
-    logger.error(err.message);
-    throw err;
-  }
-
-  // install middleware
-  swaggerExpress.register(app);
-
-  const port = process.env.PORT || 10010;
-  app.listen(port);
-
-  if (swaggerExpress.runner.swagger.paths["/hello"]) {
-    logger.info(
-      "try this:\ncurl http://127.0.0.1:" + port + "/hello?name=Scott"
-    );
-  }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/hello", getHello);
+app.use(
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+app.listen(port, () => {
+  logger.info(`The server is running at http://127.0.0.1:${port}`);
 });
+
+// SwaggerExpress.create(config, (err, swaggerExpress) => {
+//   if (err) {
+//     logger.error(err.message);
+//     throw err;
+//   }
+
+//   // install middleware
+//   swaggerExpress.reggister(app);
+
+//   const port = process.env.PORT || 10010;
+//   app.listen(port);
+
+//   if (swaggerExpress.runner.swagger.paths["/hello"]) {
+//     logger.info(`try this:\ncurl http://127.0.0.1:${port}/hello?name=Scott}`);
+//   }
+// });
 export default app;
---
+++ test/api/controllers/hello_world.test.ts
@@ -0,0 +1,35 @@
+import request from "supertest";
+import server from "../../../api/server";
+
+describe("controllers", () => {
+  describe("hello_world", () => {
+    describe("GET /hello", () => {
+      it("should return a default string", done => {
+        request(server)
+          .get("/hello")
+          // .set("Accept", "application/json")
+          // .expect("Content-Type", /json/)
+          .expect(200)
+          .end((err, res) => {
+            expect(err).toBeFalsy();
+            expect(res.body).toEqual("Hello, stranger!");
+            done();
+          });
+      });
+
+      it("should accept a name parameter", done => {
+        request(server)
+          .get("/hello")
+          .query({ name: "Scott" })
+          .set("Accept", "application/json")
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .end((err, res) => {
+            expect(err).toBeFalsy();
+            expect(res.body).toEqual("Hello, Scott!");
+            done();
+          });
+      });
+    });
+  });
+});
