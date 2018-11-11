0296e8b69028f91ec7080ccbefb528c9fb5df9d4
dontry <mccoy018@gmail.com>
2018-11-11 21:06:57 +1100

    added logging middlewares

---
+++ api/middlewares/LoggingMiddleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { Middleware, ExpressMiddlewareInterface } from "routing-controllers";
+import logger from "../../utils/logger";
+
+@Middleware({ type: "before" })
+class LoggingMiddleware implements ExpressMiddlewareInterface {
+  public use(
+    request: Request,
+    response: Response,
+    next: (err?: any) => any
+  ): void {
+    logger.info("request:", request);
+    next();
+  }
+}
+export default LoggingMiddleware;
