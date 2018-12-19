693b8c220f53bc45b0d0e7bc14d040689203e875
dontry <mccoy018@gmail.com>
2018-12-20 01:23:27 +1100

    remove regex global flag in password validation

--- src/api/validators/PasswordPattern.ts
+++ src/api/validators/PasswordPattern.ts
@@ -3,13 +3,14 @@ import {
   ValidatorConstraintInterface,
   ValidationArguments
 } from "class-validator";
+import { logger } from "../../utils";
 
 @ValidatorConstraint({ name: "PasswordPattern", async: false })
 export class PasswordPattern implements ValidatorConstraintInterface {
   // Minimum eight characters, Maximum 16 characters, at least one letter, one number and one special character:
-  private passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%.*#?&])[A-Za-z\d@.$!%*#?&]{8,24}$/g;
+  private passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%.*#?&])[A-Za-z\d@.$!%*#?&]{8,24}$/;
 
-  public validate(password: string, args: ValidationArguments): boolean {
+  public validate(password: string): boolean {
     return this.passwordRegex.test(password);
   }
 
